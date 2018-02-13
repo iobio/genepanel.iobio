c53f6fd20a259ab7c2d3d128989dcdeacbf95d01
Aditya Ekawade <aditya.ekawade@gmail.com>
2018-02-13 10:35:13 -0700

    Add radio buttons in filter panel

--- client/app/components/pages/FilterPanel.vue
+++ client/app/components/pages/FilterPanel.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
-  
+
 
     <h4>Genes</h4>
-    <btn v-on:click="SelectAllGenes"> Select All Genes</btn>
-    <btn v-on:click="deSelectAllGenes"> De Select All Genes</btn>
+    <!-- <btn v-on:click="SelectAllGenes"> Select All Genes</btn>
+    <btn v-on:click="deSelectAllGenes"> De Select All Genes</btn> -->
+    <input type="radio" name="genesSelection" value="selectAllGenes" v-on:click="SelectAllGenes">&nbsp; Select All Genes &nbsp;&nbsp;
+    <input type="radio" name="genesSelection" value="deSelectAllGenes" v-on:click="deSelectAllGenes">&nbsp; Deselect All Genes
+
 
     <br><br>
 
     <h4>Disorders</h4>
-    <btn v-on:click="SelectAllDisorders"> Select All Disorders</btn>
-    <btn v-on:click="deSelectAllDisorders"> De Select All Disorders</btn>
+    <!-- <btn v-on:click="SelectAllDisorders"> Select All Disorders</btn>
+    <btn v-on:click="deSelectAllDisorders"> De Select All Disorders</btn> -->
+
+    <input type="radio" name="disorderSelection" value="selectAllDisorders" v-on:click="SelectAllDisorders">&nbsp; Select All Disorders &nbsp;&nbsp;
+    <input type="radio" name="disorderSelection" value="deSelectAllDisorders" v-on:click="deSelectAllDisorders">&nbsp; Deselect All Disorders
+
   </div>
 </template>
 
