e361368157db0b22988012e6d0ce7f57a93dcdd1
Aditya Ekawade <aditya.ekawade@gmail.com>
2018-03-28 10:48:40 -0600

    Use keep-alive for conditional rendering of routes

--- client/app/components/pages/Main.vue
+++ client/app/components/pages/Main.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <NavigationBar></NavigationBar>
+    {{ component }}
     <div>
       <v-toolbar color="cyan" dark tabs>
         <v-tabs
@@ -24,10 +25,31 @@
 
         </v-tabs>
       </v-toolbar>
-      <v-tabs-items v-model="tab">
+      <!-- <v-tabs-items>
         <keep-alive>
           <component v-bind:is="component"></component>
         </keep-alive>
+      </v-tabs-items> -->
+      <v-tabs-items>
+        <keep-alive>
+          <Home v-if="component==='Home'"></Home>
+          <HomePage v-else-if="component==='HomePage'"></HomePage>
+          <Phenolyzer v-else-if="component==='Phenolyzer'"></Phenolyzer>
+          <SummaryTab v-else-if="component==='SummaryTab'"></SummaryTab>
+        </keep-alive>
+        <!-- <div v-if="component==='Home'">
+          <Home></Home>
+        </div>
+        <div v-else-if="component==='Phenolyzer'">
+          <Phenolyzer></Phenolyzer>
+        </div>
+        <div v-else-if="component==='HomePage'">
+          <HomePage></HomePage>
+        </div>
+        <div v-else-if="component==='SummaryTab'">
+          <SummaryTab></SummaryTab>
+        </div> -->
+
       </v-tabs-items>
     </div>
     <center>
