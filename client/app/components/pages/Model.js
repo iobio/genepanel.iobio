b66c407d3de0c77cae5718be0a833f3cb3ec6c00
Aditya Ekawade <aditya.ekawade@gmail.com>
2018-04-04 10:01:26 -0600

    Scale bar of first gene to same length in gene table

--- client/app/components/pages/Model.js
+++ client/app/components/pages/Model.js
@@ -399,7 +399,7 @@ mergeGenesAcrossPanels(genePanels) {
         return b._genePanelCount - a._genePanelCount ;
       }
     })
-    if(sortedGenes[0]._genePanelCount>90){
+    var multiplicationFactor = 900/sortedGenes[0]._genePanelCount;
       return sortedGenes.map(function(gene, idx) {
         return {
               key: idx,
@@ -407,7 +407,7 @@ mergeGenesAcrossPanels(genePanels) {
               value: +gene._genePanelCount,
               diseases: gene._diseaseCount,
               conditions: gene._diseaseNames,
-              htmlData: `<svg width="${gene._genePanelCount * 6}" height="25" xmlns="http://www.w3.org/2000/svg">
+              htmlData: `<svg width="${gene._genePanelCount * multiplicationFactor}" height="25" xmlns="http://www.w3.org/2000/svg">
       <defs>
           <linearGradient id="MyGradient">
               <stop offset="5%"  stop-color="#36D1DC"/>
@@ -416,36 +416,11 @@ mergeGenesAcrossPanels(genePanels) {
       </defs>
 
       <rect fill="url(#MyGradient)"
-            x="10" y="5" width="${gene._genePanelCount * 6}" height="25"/>
-      <text x="${gene._genePanelCount * 2}" y="20" font-family="Verdana" font-size="14" fill="white">${gene._genePanelCount}</text>
+            x="10" y="5" width="${gene._genePanelCount * multiplicationFactor}" height="25"/>
+      <text x="${gene._genePanelCount * multiplicationFactor/2.5}" y="20" font-family="Verdana" font-size="14" fill="white">${gene._genePanelCount}</text>
   </svg>`
             };
       });
-    }
-    else {
-      return sortedGenes.map(function(gene, idx) {
-        return {
-              key: idx,
-              name: gene.name,
-              value: +gene._genePanelCount,
-              diseases: gene._diseaseCount,
-              conditions: gene._diseaseNames,
-              htmlData: `<svg width="${gene._genePanelCount * 15}" height="25" xmlns="http://www.w3.org/2000/svg">
-      <defs>
-          <linearGradient id="MyGradient">
-              <stop offset="5%"  stop-color="#36D1DC"/>
-              <stop offset="95%" stop-color="#5B86E5"/>
-          </linearGradient>
-      </defs>
-
-      <rect fill="url(#MyGradient)"
-            x="10" y="5" width="${gene._genePanelCount * 15}" height="25"/>
-      <text x="${gene._genePanelCount * 6}" y="20" font-family="Verdana" font-size="14" fill="white">${gene._genePanelCount}</text>
-  </svg>`
-            };
-      });
-    }
-
 
   }
 
