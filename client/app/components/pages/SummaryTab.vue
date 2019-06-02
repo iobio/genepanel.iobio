d6811cff3bbebce5bdead6733bf9ce94b2d9948f
Aditya Ekawade <aditya.ekawade@gmail.com>
2019-06-01 19:22:55 -0700

    Add 4 way venn diagram

--- client/app/components/pages/SummaryTab.vue
+++ client/app/components/pages/SummaryTab.vue
@@ -1089,9 +1089,74 @@ import progressCircularDonut from '../partials/progressCircularDonut.vue';
         var tableGenes = [...allSourcesGenes, ...threeSourcesGenes, ...twoSourcesGenes, ...uniqueAddedGenes, ...uniqueGTR, ...uniqueClinPhen, ...uniquePheno];
         console.log("tableGenes", tableGenes);
         this.summaryTableArray = tableGenes;
-        // this.generateVennDiagramData(summaryObj);
+        this.generateVennDiagramData(summaryObj);
       },
-
+      generateVennDiagramData(summaryObj){
+        this.vennData = {
+          "data": [
+            {"sets" : [0], "label" : "GTR", "size" : summaryObj.gtr.count},
+            {"sets" : [1], "label" : "Phenolyzer", "size": summaryObj.phenolyzer.count},
+            {"sets" : [2], "label" : "Imported", "size" : summaryObj.ImportedGenes.count},
+            {"sets" : [3], "label" : "ClinPhen", "size":summaryObj.ClinPhen.count},
+            {"sets" : [0,1], "size":summaryObj.gtr_phenolyzer.count},
+            {"sets" : [0,2], "size":summaryObj.gtr_ImportedGenes.count},
+            {"sets" : [0,3], "size":summaryObj.gtr_ClinPhen.count},
+            {"sets" : [1,2], "size":summaryObj.phenolyzer_ImportedGenes.count},
+            {"sets" : [1,3], "size":summaryObj.phenolyzer_ClinPhen.count},
+            {"sets" : [2,3], "size":summaryObj.ImportedGenes_ClinPhen.count},
+            {"sets" : [0,2,3], "size":summaryObj.gtr_ImportedGenes_ClinPhen.count},
+            {"sets" : [0,1,2], "size":summaryObj.gtr_phenolyzer_ImportedGenes.count},
+            {"sets" : [0,1,3], "size":summaryObj.gtr_phenolyzer_ClinPhen.count},
+            {"sets" : [1,2,3], "size":summaryObj.phenolyzer_ImportedGenes_ClinPhen.count},
+            {"sets" : [0,1,2,3], "size":summaryObj.gtr_phenolyzer_ImportedGenes_ClinPhen}
+          ]
+        }
+        if(this.GtrGenesArr.length>0 || this.PhenolyzerGenesArr.length>0 ||  this.manuallyAddedGenes.length>0 || this.clinPhenGenesArr.length>0){
+          this.drawVennDiagram();
+        }
+        else {
+          d3.select("#venn").select("svg").remove();
+        }
+      },
+      drawVennDiagram(){
+        d3.select("#venn").select("svg").remove();
+        var x = require('venn.js')
+        var chart = x.VennDiagram()
+                 .width(400)
+                 .height(400);
+      var div = d3.select("#venn")
+      div.datum(this.vennData.data).call(chart);
+      var tooltip = d3.select("body").append("div")
+          .attr("class", "venntooltip");
+      div.selectAll("path")
+          .style("stroke-opacity", 0)
+          .style("stroke", "#fff")
+          .style("stroke-width", 3)
+      div.selectAll("g")
+          .on("mouseover", function(d, i) {
+              // sort all the areas relative to the current item
+              x.sortAreas(div, d);
+              // Display a tooltip with the current size
+              tooltip.transition().duration(400).style("opacity", .9);
+              tooltip.text(d.size + " genes");
+              // highlight the current path
+              var selection = d3.select(this).transition("tooltip").duration(400);
+              selection.select("path")
+                  .style("fill-opacity", d.sets.length == 1 ? .4 : .1)
+                  .style("stroke-opacity", 1);
+          })
+          .on("mousemove", function() {
+              tooltip.style("left", (d3.event.pageX) + "px")
+                     .style("top", (d3.event.pageY - 28) + "px");
+          })
+          .on("mouseout", function(d, i) {
+              tooltip.transition().duration(400).style("opacity", 0);
+              var selection = d3.select(this).transition("tooltip").duration(400);
+              selection.select("path")
+                  .style("fill-opacity", d.sets.length == 1 ? .25 : .0)
+                  .style("stroke-opacity", 0);
+          });
+      },
       setPieChartData(){
         this.pieChartdataArr = [
           {
@@ -1135,4 +1200,17 @@ import progressCircularDonut from '../partials/progressCircularDonut.vue';
     border-bottom: 6px solid $activeCard-border
 .dialogBox
   margin-top: -20px
+
+.venntooltip
+  position: absolute
+  text-align: center
+  width: 128px
+  height: 26px
+  background: #333
+  color: #ddd
+  padding: 2px
+  border: 0px
+  border-radius: 8px
+  opacity: 0
+
 </style>
